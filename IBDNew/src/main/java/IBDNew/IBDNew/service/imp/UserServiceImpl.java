5c6478bf244a3c56562a5d6506051f2ebea531b2
Szymon <szymon.drozdek@o2.pl>
2018-11-27 20:29:09 +0100

    null pointer handled

--- IBDNew/src/main/java/IBDNew/IBDNew/service/imp/UserServiceImpl.java
+++ IBDNew/src/main/java/IBDNew/IBDNew/service/imp/UserServiceImpl.java
@@ -1,6 +1,6 @@
 package IBDNew.IBDNew.service.imp;
 
-import org.springframework.beans.factory.annotation.Autowired;   
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import IBDNew.IBDNew.model.User;
@@ -10,67 +10,70 @@ import IBDNew.IBDNew.service.UserService;
 import javax.transaction.Transactional;
 import java.util.List;
 
-
 @Service("userServiceImpl")
 @Transactional
 public class UserServiceImpl implements UserService {
 
-    UserDao userDao;
+	UserDao userDao;
 
-    @Autowired
-    public UserServiceImpl(UserDao userDao) {
-        this.userDao = userDao;
-    }
+	@Autowired
+	public UserServiceImpl(UserDao userDao) {
+		this.userDao = userDao;
+	}
 
-    @Override
-    public User save(User user) {
-        user.setPassword(user.getPassword());
-        user.setPassword(user.getConfirmPassword());
-        return userDao.save(user);
-    }
+	@Override
+	public User save(User user) {
+		user.setPassword(user.getPassword());
+		user.setPassword(user.getConfirmPassword());
+		return userDao.save(user);
+	}
 
-    @Override
-    public User update(User user) {
-    	return userDao.save(user);
-    }
+	@Override
+	public User update(User user) {
+		return userDao.save(user);
+	}
 
-    @Override
-    public List<User> getAllUsers() {
-        return userDao.findAll();
-    }
+	@Override
+	public List<User> getAllUsers() {
+		return userDao.findAll();
+	}
 
-    @Override
-    public User getUserByEmail(String email) {
-        return userDao.findOneByEmail(email);
-    }
+	@Override
+	public User getUserByEmail(String email) {
+		return userDao.findOneByEmail(email);
+	}
 
-    @Override
-    public User getUserById(Long id) {
-    	User user =  userDao.findOneById(id);
+	@Override
+	public User getUserById(Long id) {
+		User user = userDao.findOneById(id);
 
-    	if (user == null) {
-    		return null;
-    	}
+		if (user == null) {
+			return null;
+		}
 
-    	return user;
-    }
+		return user;
+	}
 
 	@Override
 	public User findUser(User user) {
 		User newUser = userDao.findOneByEmail(user.getEmail());
-		if(newUser.getPassword().equals(user.getPassword())) {
-			return newUser;
-		}else
+		if (newUser != null) {
+			if (newUser.getPassword().equals(user.getPassword())) {
+				return newUser;
+			} else
+				return null;
+		} else
 			return null;
+
 	}
 
-//    @Override
-//    public UserDetails loadUserByUsername(String email) {
-//        User user = userDao.findOneByEmail(email);
-//        if (user == null) {
-//            throw new UsernameNotFoundException(email);
-//        }
-//        return user;
-//    }
+	// @Override
+	// public UserDetails loadUserByUsername(String email) {
+	// User user = userDao.findOneByEmail(email);
+	// if (user == null) {
+	// throw new UsernameNotFoundException(email);
+	// }
+	// return user;
+	// }
 
 }
